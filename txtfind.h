df9629ef2f670873159a1c888df769cda4a75e7c
Or Mendel <57266329+Ormendel@users.noreply.github.com>
2019-12-18 12:28:37 +0200

    Implement txtfind
    bugs incoming while getting strings via input files

---
+++ txtfind.h
@@ -0,0 +1,13 @@
+#ifndef findtxt_h
+#define findtxt_h
+
+int getchars(char c[]);
+int getlines(char s[]);
+int getwords(char w[]);
+int substring(char* str1, char* str2);
+int similar(char* s, char* t, int n);
+void print_lines(char* str);
+void print_similar_words(char* str);
+
+
+#endif
